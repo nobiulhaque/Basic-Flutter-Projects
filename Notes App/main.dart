6c4a185296b71a24ee176470a8a21646996b2464
MD. Nobiul Haque Riyad <103303700+nobiulhaque@users.noreply.github.com>
2025-06-15 17:04:52 +0600

    Add files via upload

---
+++ Notes App/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(NoatesApp());
+}
+
+class NoatesApp extends StatelessWidget {
+  const NoatesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Noates App',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: ,
+    );
+  }
+}
