8322b9114d5c293c5aa0ac39b5478879fc4d4f1e
MD. Nobiul Haque Riyad <103303700+nobiulhaque@users.noreply.github.com>
2025-06-17 15:28:17 +0600

    Update main.dart

--- Notes App/main.dart
+++ Notes App/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/screens/notes_screen.dart';
 
 void main(){
   runApp(NoatesApp());
 }
+
 
 class NoatesApp extends StatelessWidget {
   const NoatesApp({super.key});
@@ -10,11 +12,12 @@ class NoatesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Noates App',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: ,
+      home: const NotesScreen(),
     );
   }
 }
